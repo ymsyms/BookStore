5ea8caa90452781a9a6e7cf913f8b1bdd69a8c54
winston <tan.wensong.tws@gmail.com>
2017-11-20 18:48:56 +0800

    [listview added to Order Receipt]

--- Team5BookShop/OrderReceipt.aspx
+++ Team5BookShop/OrderReceipt.aspx
@@ -3,5 +3,6 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:ListView ID="ShoppingCartlv" runat="server"></asp:ListView>
 </asp:Content>
 
--- Team5BookShop/OrderReceipt.aspx.cs
+++ Team5BookShop/OrderReceipt.aspx.cs
@@ -10,5 +10,7 @@ public partial class OrderReceipt : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         ShoppingCart userCart = (ShoppingCart)Session["ShoppingCartObj"];
+        ShoppingCartlv.DataSource = userCart;
+        ShoppingCartlv.DataBind();
     }
 }
