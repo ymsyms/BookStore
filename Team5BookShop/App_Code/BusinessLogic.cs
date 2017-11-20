3e852d5d91b876d934af3dadd22906c81a877a02
yimon <yimon.soe@globalexpertteam.com>
2017-11-20 18:46:57 +0800

    [Comment out]

--- Team5BookShop/App_Code/BusinessLogic.cs
+++ Team5BookShop/App_Code/BusinessLogic.cs
@@ -38,48 +38,39 @@ public class BusinessLogic
     //03 registration page methods (by inetmgr)
 
     //04 shopping cart page methods
-    public static List<CartItem> GetShoppingCart()
-    {
-        return shoppingCart;
-    }
-
-    //EditCartItem (in CodeBehind)
-
-    public static decimal GetShoppingCartTotalPrice()
-    {
-        return shoppingCart.TotalPrice();
-    }
-
-    public static void Checkout(string userID, string mailingAddress, DateTime orderDate, decimal totalPrice)
-    {
-        using (BookshopEntities context = new BookshopEntities())
-        {
-            //Create Order
-            Order order = new Order();
-            order.UserID = userID;
-            order.MailingAddress = mailingAddress;
-            order.OrderDate = orderDate;
-            order.TotalPrice = totalPrice;
-            context.Orders.Add(order);
-            context.SaveChanges();
-
-            //Create OrderDetails from CartItems
-            foreach (CartItem item in shoppingCart)
-            {
-                OrderDetail orderDetail = new OrderDetail();
-                orderDetail.OrderID = context.Orders.Max(x => x.OrderID);
-                orderDetail.BookID = item.BookID;
-                orderDetail.Quantity = item.Quantity;
-                orderDetail.UnitPrice = item.UnitPrice;
-                orderDetail.SubTotalPrice = item.SubTotal;
-                context.OrderDetails.Add(orderDetail);
-            }
-            context.SaveChanges();
-
-            //Clear ShoppingCart
-            shoppingCart.Clear();
-        }
 
 
-    }
+
+    //public static void Checkout(string userID, string mailingAddress, DateTime orderDate, decimal totalPrice, ShoppingCart shoppoingCart)
+    //{
+    //    using (BookshopEntities context = new BookshopEntities())
+    //    {
+    //        //Create Order
+    //        Order order = new Order();
+    //        order.UserID = userID;
+    //        order.MailingAddress = mailingAddress;
+    //        order.OrderDate = orderDate;
+    //        order.TotalPrice = totalPrice;
+    //        context.Orders.Add(order);
+    //        context.SaveChanges();
+
+    //        //Create OrderDetails from CartItems
+    //        foreach (CartItem item in shoppingCart)
+    //        {
+    //            OrderDetail orderDetail = new OrderDetail();
+    //            orderDetail.OrderID = context.Orders.Max(x => x.OrderID);
+    //            orderDetail.BookID = item.BookID;
+    //            orderDetail.Quantity = item.Quantity;
+    //            orderDetail.UnitPrice = item.UnitPrice;
+    //            orderDetail.SubTotalPrice = item.SubTotal;
+    //            context.OrderDetails.Add(orderDetail);
+    //        }
+    //        context.SaveChanges();
+
+    //        //Clear ShoppingCart
+    //        shoppingCart.Clear();
+    //    }
+
+
+    //}
 }
