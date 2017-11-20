bb300cc2bb7ec48587ede810301fc4a72020b106
Zanne333 <30918182+Zanne333@users.noreply.github.com>
2017-11-20 19:38:20 +0800

    BookBusiness Logic

---
+++ Team5BookShop/App_Code/BookBusinesslogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for BookBusinesslogic
+/// </summary>
+public class BookBusinesslogic
+{
+    public BookBusinesslogic()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
