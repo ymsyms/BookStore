2701844c6d4dc12507c4d84bbb9ea66bd42c8f91
winston <tan.wensong.tws@gmail.com>
2017-11-20 18:35:08 +0800

    [access ShoppingCartObj for OrderReceipt]

--- Team5BookShop/OrderReceipt.aspx.cs
+++ Team5BookShop/OrderReceipt.aspx.cs
@@ -9,6 +9,6 @@ public partial class OrderReceipt : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        ShoppingCart userCart = (ShoppingCart)Session["ShoppingCartObj"];
     }
 }
