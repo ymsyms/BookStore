1c61c68e379f0a378620688d914928fdefe0fcea
yimon <yimon.soe@globalexpertteam.com>
2017-11-21 10:47:23 +0800

    [Login Function]

--- Team5BookShop/LogIn.aspx.cs
+++ Team5BookShop/LogIn.aspx.cs
@@ -7,8 +7,28 @@ using System.Web.UI.WebControls;
 
 public partial class LogIn : System.Web.UI.Page
 {
+    UserBusinessLogic userBusinessLogic = new UserBusinessLogic();
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string userName = TextBox1.Text;
+        string password = Password1.Value;
+        bool isValid = userBusinessLogic.LoginVerification(userName, password);
+
+        if(isValid)
+        {
+            Session["userId"] = userName;
+            Label3.Text = "Success User";
+            Response.Redirect("~/Main.aspx");
+        }
+        else
+        {
+            Label3.Text = "Invalid User";
+        }    
+    }
+
 }
